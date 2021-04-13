320eba3c27bce8409d1f71ceb65517f2c5433454
Artem Golovin <artempotapkin96@icloud.com>
2021-04-13 18:29:50 +0300

    MVP 2.0

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 // Кнопка сортировки
 let sortImg = document.querySelector('.sortbutton');
+let  delButton = document.querySelectorAll('.task-button');
+let firstDeleteButton = document.querySelector('.task-button')
 
-sortImg.addEventListener('mouseover', (event) => {
+    sortImg.addEventListener('mouseover', (event) => {
         if (sortImg.src.endsWith('/img/todo_down_svg.svg')) {
             sortImg.src = './img/sort_down_black.svg';
         } else if (sortImg.src.endsWith('/img/sort_up_svg.svg')) {
@@ -17,28 +19,38 @@ sortImg.addEventListener('mouseout', (event) => {
         }
 });
 
-sortImg.addEventListener('click', (event) => {
-    console.log(sortImg.src)
+
+function sorEventLisImg () {
+    
     if (sortImg.src.endsWith('/img/sort_down_black.svg')) {
         sortImg.src = './img/sort_up_black.svg';
-        sortList()
+        sortList();
     } else if (sortImg.src.endsWith('/img/sort_up_black.svg')) {
         sortImg.src = './img/sort_down_black.svg';
-        sortListReverse()
+        sortListReverse();
     }
-});
+}
+
+sortImg.addEventListener('click', sorEventLisImg);
+
+
 
 // Кнопка удалить 
-// Функция для создания обработчика вместе с родительским div'ом
-function deleteButton() {
-    let  delButton = document.querySelectorAll('.task-button');
-    delButton.forEach((el) => {
-        el.addEventListener('click', (event) => {
+// "Вешаем" обработчик на кнопку удаления
+function addEvListenerClickDelete (arg) {
+    arg.addEventListener('click', (event) => {
+        arg.parentNode.remove();
+    })
+    arg.addEventListener('keydown', (event) => {
+        if (event.keyCode == 32 && arg == document.activeElement) {
             event.preventDefault();
-            el.parentNode.remove();
-        })
+            arg.parentNode.remove();
+
+        }
     })
 }
+addEvListenerClickDelete(firstDeleteButton); //"Вешае" на первую кнопку удаления
+
 
 
 // Кнопка добавить 
@@ -47,29 +59,24 @@ let div = document.querySelector('.tasks'); // Копируемый элемен
 let list = document.querySelector('.list');
 let input = document.querySelector('.task-text')
 
-
-addButton.addEventListener('mouseover', (event) => {
-        if (sortImg.src.endsWith('/img/add_svg.svg')) {
-            sortImg.src = './img/add_off_svg.svg';
-        } else if (sortImg.src.endsWith('/img/add_off_svg.svg')) {
-            sortImg.src = './img/add_svg.svg';
+    addButton.addEventListener('click', (event) => {
+        let cloneInput = div.cloneNode(true);
+        cloneInput.firstElementChild.value = '';
+        addEvListenerFocusBlurToDelButton(cloneInput);
+        addEvListenerClickDelete (cloneInput.lastElementChild);
+        list.append(cloneInput);
+    });
+//Обработчик сработает при фокусе на кнопке и нажатии пробела
+    addButton.addEventListener('keydown', (event) => {
+        if (event.keyCode == 32 && addButton == document.activeElement) {
+            let cloneInput = div.cloneNode(true);
+            cloneInput.firstElementChild.value = '';
+            addEvListenerFocusBlurToDelButton(cloneInput);
+            addEvListenerClickDelete (cloneInput.lastElementChild);
+            list.append(cloneInput);
         }
-});
+    });
 
-addButton.addEventListener('mouseout', (event) => {
-    if (sortImg.src.endsWith('/img/add_svg.svg')) {
-        sortImg.src = './img/add_off_svg.svg';
-    } else if (sortImg.src.endsWith('/img/add_off_svg.svg')) {
-        sortImg.src = './img/add_svg.svg';
-    }
-});
-
-addButton.addEventListener('click', (event) => {
-    let cloneInput = div.cloneNode(true);
-    cloneInput.firstElementChild.value = ''
-    list.append(cloneInput);
-    deleteButton();
-});
 
 // Сортировка списка задач по алфавиту в прямом порядке
 function sortList() {
@@ -117,6 +124,53 @@ function sortListReverse() {
         list.append(el)
     })
 
+}   
+
+// Адаптируем приложение для людей с ограничением по зрению
+//Анимация при выборе клавишей TAB
+function addEvListenerFocusBlurToDelButton (argument) {
+        argument.lastElementChild.addEventListener('focus', (event) => {
+                argument.lastElementChild.style.background = 'url("img/delete_on_svg.svg")';
+                argument.lastElementChild.style.backgroundSize = '97%';
+              });
+        argument.lastElementChild.addEventListener('blur', (event) => {
+            argument.lastElementChild.style.background = 'none';
+            argument.lastElementChild.src = './img/delete_off_svg.svg';
+            });
+
 }
 
+sortImg.addEventListener('focus', (event) => {
+    sortImg.src = './img/sort_down_black.svg';
+  });
+
+sortImg.addEventListener('blur', (event) => {
+    sortImg.src = './img/todo_down_svg.svg';
+  });
+
+
+
+//"Ловим" события клавиатуры
+
+function keyPress(e) {
+    let keyNum;
+    if (window.event) {
+        keyNum = window.event.keyCode;
+    }
+    else if (e) {
+        keyNum = e.which;
+    }
+}
+document.onkeydown = keyPress;
+
+document.onkeydown = function(event){
+    if (event.keyCode == 32 && sortImg == document.activeElement) {
+        event.preventDefault();
+        sorEventLisImg ()
+    }
+
+    delButton.forEach((el) => {
+
+    })
+};
 
